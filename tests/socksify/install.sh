f3e317c2e8aa74aa2c9389f49d31cbdc97ad4ba4
stack zhao <zyh001@users.noreply.github.com>
2017-07-31 11:00:17 +0800

    Update install.sh

--- tests/socksify/install.sh
+++ tests/socksify/install.sh
@@ -5,6 +5,7 @@ if [ ! -d dante-1.4.0 ]; then
     tar xf dante-1.4.0.tar.gz || exit 1
 fi
 pushd dante-1.4.0
-./configure && make -j4 && make install || exit 1
+cd dante-1.4.0
+sudo bash ./configure && make -j4 && make install || exit 1
 popd
 cp tests/socksify/socks.conf /etc/ || exit 1
